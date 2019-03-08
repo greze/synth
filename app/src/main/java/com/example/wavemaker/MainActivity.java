db871738ab622c1c18610c53dc43c306f3969d67
greze <lmgiles6@gmail.com>
2019-03-07 21:23:17 -0600

    initial commit

---
+++ app/src/main/java/com/example/wavemaker/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.wavemaker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    private native void touchEvent(int action);
+
+    private native void startEngine();
+
+    private native void stopEngine();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startEngine();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        touchEvent(event.getAction());
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void onDestroy() {
+        stopEngine();
+        super.onDestroy();
+    }
+}
